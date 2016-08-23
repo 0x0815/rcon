2661945e4c8982597fed6c008656e7e00b5464e7
Administrator <administrator@macbook-pro-3.local>
2016-08-23 15:29:05 +0200

    Initial Commit

---
+++ rcon/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  rcon
+//
+//  Created by Administrator on 23.08.16.
+//  Copyright © 2016 DontPlayAlone.de. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
