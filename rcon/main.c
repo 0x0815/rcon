8421e3912ed9523c6b217a67c6e842c1aef83001
Administrator <administrator@macbook-pro-3.local>
2016-08-23 15:48:56 +0200

    Initial Commit

--- rcon/main.c
+++ rcon/main.c
@@ -2,14 +2,45 @@
 //  main.c
 //  rcon
 //
-//  Created by Administrator on 23.08.16.
+//  Created by xass on 22.08.16.
 //  Copyright © 2016 DontPlayAlone.de. All rights reserved.
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include "rcon.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+#define QUIET false
+#define DEBUG true
+
+int main(int argc, char *argv[]) {
+    
+    if (argc <= 0) {
+        printf("%s \n","Usage: rcon {ip} {port} {password}");
+        exit(0);
+    }
+    
+    if(!isValidIpAddress(argv[1])){
+        printf("%s \n","Not a valid IP.");
+        exit(0);
+    }
+    
+    if (!isdigit(argv[2])) {
+        printf("%s \n","Not a valid port.");
+    }
+}
+
+bool isValidIpAddress(char *ipAddress){
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ipAddress, &(sa.sin_addr));
+    return result != 0;
 }
---
+++ rcon/rcon.h
@@ -0,0 +1,14 @@
+//
+//  rcon.h
+//  rcon
+//
+//  Created by Administrator on 23.08.16.
+//  Copyright © 2016 DontPlayAlone.de. All rights reserved.
+//
+
+#ifndef rcon_h
+#define rcon_h
+
+bool isValidIpAddress(char *ipAddress);
+
+#endif /* rcon_h */
